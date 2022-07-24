1410b4cca393dc956656539edaece8982452e6a9
Kittipod Lambangchang <tonkittipod@gmail.com>
2022-07-24 22:52:27 +0700

    🌠 add : new files

--- rush/sdachdac/script.js
+++ rush/sdachdac/script.js
@@ -1,4 +1,17 @@
-window.addEventListener("scroll",function(){
-  var nav = this.document.querySelector("nav")
-  nav.classList.toggle("sticky", window.scrollY > 0)
+let open = false
+document.getElementById("toggle").addEventListener('click', () => {
+
+  open = !open
+  if (open) {
+    document.getElementById("menu-slide").style.top = "65px"
+    document.getElementById("toggle").classList.remove("fa-bars")
+    document.getElementById("toggle").classList.add("fa-xmark")
+  } else {
+    document.getElementById("menu-slide").style.top = "-1000px";
+    document.getElementById("toggle").classList.add("fa-bars")
+    document.getElementById("toggle").classList.remove("fa-xmark")  
+  }
 })
+
+
+document.getElementById("footer").innerHTML = `&copy; Copyright Sdachdac ${new Date().getFullYear()}`
